ccc63848d5911235f612dea915028a8b188fa48d
davidRichard23 <d2391@me.com>
2021-06-16 13:28:40 -0800

    depend on hummhive gatsby plugin to load initial state

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,23 +7,11 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { HummContext } from "gatsby-plugin-hummhive-react-web-data"
 import Header from "./header"
 import "../../static/bootstrap.min.css"
 import "../../static/layout.css"
 
 const Layout = ({ children, header }) => {
-  const { state, actions } = React.useContext(HummContext)
-
-  React.useEffect(() => {
-    if (!state.hive) init()
-  }, [])
-
-  const init = () => {
-    actions.getHive()
-    actions.getMe()
-  }
-
   return (
     <>
       {header !== "no" && <Header />}
